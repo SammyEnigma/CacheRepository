e192546f1ef56a8b8f1a9f739c5087959b976ab4
pinopino <myhinata@126.com>
2019-02-25 22:06:45 +0800

    准备试着支持带谓词条件查询

---
+++ CacheRepository/ExpressionParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace CacheRepository
+{
+    class ExUser
+    {
+        public int Id;
+        public string Name;
+    }
+
+    class ExprVisitor : ExpressionVisitor
+    {
+        public string MemberName { get; private set; }
+        public Type MemberType { get; private set; }
+        public object MemberValue { get; private set; }
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            if (node.Left is MemberExpression && node.Right is ConstantExpression)
+            {
+                var member = node.Left as MemberExpression;
+                var constant = node.Right as ConstantExpression;
+                Console.WriteLine("expression tree: Name={0}, Type={1}, Value={2}", member.Member.Name, member.Type, constant.Value);
+
+            }
+            return base.VisitBinary(node);
+        }
+
+        public void Test()
+        {
+            Expression<Func<ExUser, object>> exp = p => !((p.Id > 1 && p.Id < 20) || p.Name == "abc");
+            Visit(exp);
+        }
+    }
+}
