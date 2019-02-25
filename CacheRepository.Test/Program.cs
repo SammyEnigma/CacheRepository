1bd08c020a9ecfa5cad2bd15eb58e120e9b422fe
pinopino <myhinata@126.com>
2019-02-25 23:26:50 +0800

    临时保存工作

---
+++ CacheRepository.Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CacheRepository.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var e = new ExprVisitor();
+            e.Test();
+            Console.Read();
+        }
+    }
+}
--- CacheRepository/ExpressionParser.cs
+++ CacheRepository/ExpressionParser.cs
@@ -1,22 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace CacheRepository
 {
-    class ExUser
+    public class ExUser
     {
         public int Id;
         public string Name;
     }
 
-    class ExprVisitor : ExpressionVisitor
+    public class ExprVisitor : ExpressionVisitor
     {
         public string MemberName { get; private set; }
         public Type MemberType { get; private set; }
         public object MemberValue { get; private set; }
 
+        private List<Expression> _cache = new List<Expression>();
+        private List<int> _hash = new List<int>();
+
         protected override Expression VisitBinary(BinaryExpression node)
         {
+            if (_hash.Contains(node.GetHashCode()))
+            {
+
+            }
             if (node.Left is MemberExpression && node.Right is ConstantExpression)
             {
                 var member = node.Left as MemberExpression;
@@ -27,10 +35,21 @@ namespace CacheRepository
             return base.VisitBinary(node);
         }
 
+        protected override Expression VisitUnary(UnaryExpression node)
+        {
+            if (node.NodeType != ExpressionType.Convert)
+            {
+                _cache.Add(node.Operand);
+                _hash.Add(node.Operand.GetHashCode());
+            }
+            return base.VisitUnary(node);
+        }
+
         public void Test()
         {
-            Expression<Func<ExUser, object>> exp = p => !((p.Id > 1 && p.Id < 20) || p.Name == "abc");
-            Visit(exp);
+            Expression<Func<ExUser, object>> exp1 = p => !(!(p.Id > 1 && p.Id < 20) || p.Name == "abc");
+            Expression<Func<ExUser, object>> exp2 = p => !(p.Id == 1 || p.Id == 2 || p.Id == 3);
+            Visit(exp2);
         }
     }
 }
