df9345d3555509b56888035236925733d0e67da4
pinopino <myhinata@126.com>
2019-03-18 21:43:18 +0800

    writeback自持-0.1

---
+++ CacheRepository/IWriteBack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CacheRepository
+{
+    // 说明：通过抽象出该接口，让cacherepository能够比较容易支持
+    // 数据同步的功能；对，缓存中数据同步是通过WriteBack的方式来
+    // 实现的；
+    // BackWritter : IWriteBack
+    public interface IWriteBack
+    {
+        bool WriteBack();
+    }
+}
